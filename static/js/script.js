0131eb55a9d4a3109ee6441d29b33762f0dc7033
Nithin harsha <36791316+Nithin0111@users.noreply.github.com>
2020-10-22 20:40:47 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
     $('#ajaxloading').show();
     $('#reload').show();
   });
-  $('#title').text("Dynamic Quiz");
+  $('#title').text("Teacher Student Quiz");
   $('#title').hide().fadeIn("slow");
   $('#nameForm').hide().fadeIn("slow");
   $('#description').hide();
